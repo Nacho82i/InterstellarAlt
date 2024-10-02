41bc69fcbc45f8c20a036cd6a151be95d20cce6c
Nacho82i <142832896+Nacho82i@users.noreply.github.com>
2024-10-02 07:42:40 -0400

    Create main.bash

---
+++ main.bash
@@ -0,0 +1,5 @@
+git clone https://github.com/UseInterstellar/Interstellar
+cd Interstellar
+git pull --force --allow-unrelated-histories # This may overwrite your local changes
+npm i
+npm run start
